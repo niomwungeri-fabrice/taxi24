d07595e75e633058231937212deac65af6830f49
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2019-12-09 10:47:02 +0200

    get all available drivers

--- src/main/java/com/taxi24/dao/DriverRepo.java
+++ src/main/java/com/taxi24/dao/DriverRepo.java
@@ -1,5 +1,6 @@
 package com.taxi24.dao;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,6 @@ import com.taxi24.models.Driver;
 
 @RepositoryRestResource(collectionResourceRel = "drivers", path ="drivers")
 public interface DriverRepo extends JpaRepository<Driver, UUID>{
-
+	
+	List<Driver> findByIsAvailableTrue();
 }
