bb9d3b37d1f50f97e30226901bcda0a6b946237e
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2019-12-23 11:01:29 +0200

    add createTrip and findOneTrip feature

---
+++ src/main/java/com/taxi24/dao/TripRepo.java
@@ -0,0 +1,12 @@
+package com.taxi24.dao;
+
+
+import com.taxi24.models.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface TripRepo extends JpaRepository<Trip, UUID> {
+
+}
