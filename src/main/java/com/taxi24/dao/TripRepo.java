72ec2fd7f623515cf2283b56fd5f553a3783e898
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2020-03-27 09:16:12 +0200

    add references

--- src/main/java/com/taxi24/dao/TripRepo.java
+++ src/main/java/com/taxi24/dao/TripRepo.java
@@ -4,9 +4,11 @@ package com.taxi24.dao;
 import com.taxi24.models.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TripRepo extends JpaRepository<Trip, UUID> {
-
+    public List<Trip> findByIsCompleteFalse();
 }
